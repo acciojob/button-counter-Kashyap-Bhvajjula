e10bbb527f9789f6fe5997861b6a088ccd2eb1e6
Kashyap Bhyravavajjula <bh.kashyap.b@gmail.com>
2024-09-19 14:40:59 +0530

    Commit #2

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
-
 import React from "react";
-import './../styles/App.css';
-import Counter from "./Counter";
+import "./../styles/App.css";
+
 
 const App = () => {
   return (
     <div>
-        <Counter />
-    </div>
-  )
-}
+      const [count, setCount] = useState(0) function buttonClickHandler(e){" "}
+      {
+        // count++;
 
-export default App
+        setCount(count + 1)
+      }
+      return (
+      <div>
+        <p>Button clicked {count} times </p>
+        <button onClick={buttonClickHandler}>Click me</button>
+      </div>
+      )
+    </div>
+  );
+};
+
+export default App;
